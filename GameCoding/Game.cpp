b7d12cffd3b79670d8ace73a3345b089c1c2892f
yongilkim01 <yongil.k01@gmail.com>
2024-12-02 06:14:59 +0900

    feat: implement create device and swapchain method

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -14,6 +14,8 @@ void Game::Init(HWND hWnd)
 	_hWnd = hWnd;
 	_width = GWinSizeX;
 	_height = GWinSizeY;
+
+	CreateDeviceAndSwapChain();
 }
 
 void Game::Update()
@@ -21,5 +23,56 @@ void Game::Update()
 }
 
 void Game::Render()
+{
+}
+
+void Game::CreateDeviceAndSwapChain()
+{
+	DXGI_SWAP_CHAIN_DESC desc;
+	ZeroMemory(&desc, sizeof(desc));
+	{
+		desc.BufferDesc.Width = _width;
+		desc.BufferDesc.Height = _height;
+		desc.BufferDesc.RefreshRate.Numerator = 60;
+		desc.BufferDesc.RefreshRate.Denominator = 1;
+		desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+		desc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+		desc.SampleDesc.Count = 1;
+		desc.SampleDesc.Quality = 0;
+		desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+		desc.BufferCount = 1;
+		desc.OutputWindow = _hWnd;
+		desc.Windowed = true;
+		desc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+	}
+
+	HRESULT hr = ::D3D11CreateDeviceAndSwapChain(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE, // 그래픽카드를 사용
+		nullptr,
+		0,
+		nullptr,
+		0,
+		D3D11_SDK_VERSION,
+		&desc,
+		_swapChain.GetAddressOf(),
+		_device.GetAddressOf(),
+		nullptr,
+		_deviceContext.GetAddressOf()
+	);
+
+	CHECK(hr);
+}
+
+void Game::CreateRenderTargetView()
+{
+}
+
+void Game::RenderBegin()
+{
+}
+
+void Game::RenderEnd()
 {
 }
--- GameCoding/Game.h
+++ GameCoding/Game.h
@@ -13,8 +13,18 @@ public:
 protected:
 
 private:
+	void CreateDeviceAndSwapChain();
+	void CreateRenderTargetView();
+
+	void RenderBegin();
+	void RenderEnd();
+
 	HWND _hWnd;
 	uint32 _width = 0;
 	uint32 _height = 0;
+
+	ComPtr<ID3D11Device> _device = nullptr;
+	ComPtr<ID3D11DeviceContext> _deviceContext = nullptr;
+	ComPtr<IDXGISwapChain> _swapChain = nullptr;
 };
 
